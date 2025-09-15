8c45d3594fde9ebaf17d494b4983340eb9d3f1d1
Chris McCormick <chris@mccormick.cx>
2025-09-15 20:43:41 +0800

    Add a bunch of scenery models.

--- main.cljs
+++ main.cljs
@@ -8,6 +8,16 @@
 
 (defonce state (atom {}))
 
+(def scenery-models
+  ["tree_cone.glb" "tree_default.glb" "tree_oak.glb" "tree_pineRoundA.glb"
+   "tree_detailed.glb" "tree_fat.glb" "tree_simple.glb"
+   "plant_bush.glb" "plant_bushLarge.glb" "grass.glb" "grass_large.glb"
+   "rock_largeA.glb" "rock_largeB.glb" "rock_smallA.glb" "rock_smallB.glb"
+   "stone_largeA.glb" "stone_largeB.glb" "stone_smallA.glb" "stone_smallB.glb"
+   "stump_round.glb" "stump_old.glb"
+   "mushroom_red.glb" "mushroom_tan.glb"
+   "flower_redA.glb" "flower_purpleB.glb" "flower_yellowC.glb"])
+
 (defn apply-shadows [gltf]
   (-> gltf .-scene (.traverse (fn [node]
                                 (when (.-isMesh node)
@@ -18,6 +28,25 @@
                                     (set! (.-metalness (.-material node)) 0.01)
                                     (set! (.-roughness (.-material node)) 0.4))))))
   gltf)
+
+(defn load-and-place-scenery [scene loader model-name]
+  (let [path (str "kenney_nature-kit/Models/GLTF format/" model-name)]
+    (.load loader path
+           (fn [gltf]
+             (let [model (apply-shadows gltf)
+                   scene-obj (.-scene model)
+                   angle (* (js/Math.random) js/Math.PI 2)
+                   radius (+ 4 (* (js/Math.random) 3))
+                   x (* radius (js/Math.cos angle))
+                   z (* radius (js/Math.sin angle))
+                   scale (if (.startsWith model-name "tree")
+                           (+ 1 (* (js/Math.random) 2))
+                           1)]
+               (-> scene-obj .-position (.set x 0 z))
+               (-> scene-obj .-scale (.set scale scale scale))
+               (-> scene-obj .-rotation
+                   (.set 0 (* (js/Math.random) js/Math.PI 2) 0))
+               (.add scene scene-obj))))))
 
 (defn on-window-resize []
   (let [{:keys [camera renderer]} @state]
@@ -61,9 +90,9 @@
         _ (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
         _ (.appendChild (.-body js/document) (.-domElement renderer))
 
-        _ (.add scene (THREE/AmbientLight. 0xffffff 3.0))
+        _ (.add scene (THREE/AmbientLight. 0xffffff 0.75))
 
-        _ (.add scene (doto (THREE/SpotLight. 0xffffff 3.0)
+        _ (.add scene (doto (THREE/SpotLight. 0xffffff 1.0)
                         (-> .-position (.set 10 20 10))
                         (aset "castShadow" true)
                         (-> .-shadow .-mapSize (.set 1024 1024))))
@@ -110,6 +139,9 @@
                                                       (- (.-z center))))
                        (.add scene scene-obj)
                        (swap! state assoc :model scene-obj :model-base-y base-y))
+
+                     (dotimes [_ 15]
+                       (load-and-place-scenery scene loader (rand-nth scenery-models)))
 
                      ; Update controls to look at the model's new center
                      (.updateWorldMatrix scene-obj true)
