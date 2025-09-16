e7538af07402fc6b8d48f981a98f51434c842156
Chris McCormick <chris@mccormick.cx>
2025-09-16 11:09:08 +0800

    Dynamic model switching.

--- main.cljs
+++ main.cljs
@@ -1,5 +1,6 @@
 (ns main
-  (:require ["THREE" :as THREE]
+  (:require [promesa.core :as p]
+            ["THREE" :as THREE]
             ["OrbitControls" :as OrbitControls]
             ["GLTFLoader" :as GLTFLoader]))
 
@@ -7,16 +8,6 @@
 (def pixel-size 4)
 
 (defonce state (atom {}))
-
-(def scenery-models
-  ["tree_cone.glb" "tree_default.glb" "tree_oak.glb" "tree_pineRoundA.glb"
-   "tree_detailed.glb" "tree_fat.glb" "tree_simple.glb"
-   "plant_bush.glb" "plant_bushLarge.glb" "grass.glb" "grass_large.glb"
-   "rock_largeA.glb" "rock_largeB.glb" "rock_smallA.glb" "rock_smallB.glb"
-   "stone_largeA.glb" "stone_largeB.glb" "stone_smallA.glb" "stone_smallB.glb"
-   "stump_round.glb" "stump_old.glb"
-   "mushroom_red.glb" "mushroom_tan.glb"
-   "flower_redA.glb" "flower_purpleB.glb" "flower_yellowC.glb"])
 
 (defn apply-shadows [gltf & {:keys [brighten]}]
   (-> gltf .-scene
@@ -32,24 +23,64 @@
                        (set! (.-roughness (.-material node)) 0.4))))))
   gltf)
 
-(defn load-and-place-scenery [scene loader model-name]
-  (let [path (str "kenney_nature-kit/Models/GLTF format/" model-name)]
-    (.load loader path
+(defn set-loading [loading?]
+  (let [spinner (.getElementById js/document "spinner")]
+    (when spinner
+      (set! (.-style spinner "display") (if loading? "block" "none")))))
+
+(defn load-model [model-path]
+  (set-loading true)
+  (let [{:keys [scene loader model]} @state]
+    (when model
+      (.remove scene model))
+    (.load loader
+           (str "models/" model-path)
            (fn [gltf]
-             (let [model (apply-shadows gltf)
+             (let [model (apply-shadows gltf {:brighten 5})
                    scene-obj (.-scene model)
-                   angle (* (js/Math.random) js/Math.PI 2)
-                   radius (+ 4 (* (js/Math.random) 3))
-                   x (* radius (js/Math.cos angle))
-                   z (* radius (js/Math.sin angle))
-                   scale (if (.startsWith model-name "tree")
-                           (+ 1 (* (js/Math.random) 2))
-                           1)]
-               (-> scene-obj .-position (.set x 0 z))
-               (-> scene-obj .-scale (.set scale scale scale))
-               (-> scene-obj .-rotation
-                   (.set 0 (* (js/Math.random) js/Math.PI 2) 0))
-               (.add scene scene-obj))))))
+                   static (not (.includes model-path "float"))
+                   temp-box (doto (THREE/Box3.) (.setFromObject scene-obj))
+                   size (.getSize temp-box (THREE/Vector3.))
+                   max-dim (js/Math.max (.-x size) (.-y size) (.-z size))
+                   scale-factor (if (> max-dim 0) (/ 5 max-dim) 1)
+                   _ (-> scene-obj .-scale
+                         (.set scale-factor scale-factor scale-factor))
+                   box (doto (THREE/Box3.) (.setFromObject scene-obj))
+                   center (.getCenter box (THREE/Vector3.))]
+               (let [base-y (- (- (.-y (.-min box))
+                                  (if static 0 1)))]
+                 (-> scene-obj .-position (.set (- (.-x center))
+                                                base-y
+                                                (- (.-z center))))
+                 (.add scene scene-obj)
+                 (swap! state assoc
+                        :model scene-obj
+                        :static static
+                        :model-base-y base-y)
+                 (let [{:keys [controls]} @state]
+                   (.updateWorldMatrix scene-obj true)
+                   (let [new-box (doto (THREE/Box3.) (.setFromObject scene-obj))
+                         new-center (.getCenter new-box (THREE/Vector3.))]
+                     (-> controls .-target (.copy new-center))
+                     (.update controls))))
+               (set-loading false))))))
+
+(defn change-model [delta]
+  (let [{:keys [models current-model-index]} @state]
+    (when (pos? (count models))
+      (let [new-index (mod (+ current-model-index delta) (count models))]
+        (when (not= new-index current-model-index)
+          (swap! state assoc :current-model-index new-index)
+          (load-model (nth models new-index)))))))
+
+(defn handle-key-down [event]
+  (case (.-key event)
+    ("ArrowRight" "PageDown" " " "Enter") (change-model 1)
+    ("ArrowLeft" "PageUp") (change-model -1)
+    nil))
+
+(defn handle-tap [_event]
+  (change-model 1))
 
 (defn on-window-resize []
   (let [{:keys [camera renderer]} @state]
@@ -87,123 +118,68 @@
   (js/console.log "init...")
   (let [scene (THREE/Scene.)
         _ (set! (.-background scene) (THREE/Color. 0x303030))
-        ; _ (aset scene "fog" (THREE/FogExp2. 0xf0f0f0 0.08))
-
         camera (THREE/PerspectiveCamera.
                  70 (/ (.-innerWidth js/window)
                        (.-innerHeight js/window))
                  0.1 100)
         _ (-> camera .-position (.set 5 5 5))
-
         renderer (THREE/WebGLRenderer. #js {:antialias false})
         _ (set! (.. renderer -shadowMap -enabled) true)
         _ (set! (.. renderer -shadowMap -type) THREE/PCFShadowMap)
         _ (set! (.-toneMapping renderer) THREE/NoToneMapping)
-        ; _ (set! (.-toneMappingExposure renderer) 1.25)
         _ (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
         _ (.appendChild (.-body js/document) (.-domElement renderer))
-
         _ (.add scene (THREE/AmbientLight. 0xffffff 0.75))
-
         _ (.add scene (doto (THREE/SpotLight. 0xffffff 1.0)
                         (-> .-position (.set 10 20 10))
                         (aset "castShadow" true)
                         (-> .-shadow .-mapSize (.set 1024 1024))))
-
         _ (.add scene (doto (THREE/DirectionalLight. 0xffffff 0.5)
                         (-> .-position (.set -20 20 20))))
-
         floor (doto (THREE/Mesh. (THREE/PlaneGeometry. 20 20)
                                  (THREE/ShadowMaterial. #js {:opacity 0.3}))
                 (-> .-rotation (aset "x" (* -0.5 js/Math.PI)))
                 (aset "receiveShadow" true))
         _ (.add scene floor)
-
-        #_#_ _
-        (let [cube-geometry (THREE/BoxGeometry. 1 1 1)
-              cube-material (THREE/MeshStandardMaterial. #js {:color 0xff0000})
-              cube (doto (THREE/Mesh. cube-geometry cube-material)
-                     (-> .-position (.set 3 0.5 0))
-                     (aset "castShadow" true)
-                     (aset "receiveShadow" true))]
-          (.add scene cube))
-
         controls (OrbitControls. camera (.-domElement renderer))
         _ (-> controls .-target (.set 0 0.5 0))
         _ (.update controls)
-
-        loader (GLTFLoader.)
-        _
-        (let [[model-file static]
-              ; light spaceship
-              #_ ["models/replicate-prediction-xfexaea6f5rj00cs9j5vqpzsx0-0.glb"
-                  false]
-              ; dark spaceship
-              ["models/replicate-prediction-b43jyjw0k9rj40cs9c7rq3nt80-0.glb"
-                  false]
-              ; cool spaceship
-              #_ ["models/replicate-prediction-j9xw2dxvcxrj00cs9cj9m5pt38-0.glb"
-               false]
-              ; tokyo building
-              #_ ["models/replicate-prediction-py91pvzmb5rj20cs9eg8tyejv8-0.glb"
-                  true]
-              ; tokyo building
-              #_ ["models/replicate-prediction-qpftgart15rj60cs9ej8erp5wm-0.glb"
-                  true]
-              ; treehouse
-              #_ ["models/replicate-prediction-4pqha9va01rj40cs9f6bsjjcxr-0.glb"
-                  true]
-              ; ancient temple
-              #_ ["models/replicate-prediction-bfxzgnmm95rj00cs9eeab5v898-0.glb"
-                  true]
-              ; round temple
-              #_ ["models/replicate-prediction-vyt1pasxj5rj00cs9ehb977ajr-0.glb"
-                  true]]
-          (.load loader
-                 model-file
-                 (fn [gltf]
-                   (let [model (apply-shadows gltf {:brighten 5})
-                         scene-obj (.-scene model)
-                         temp-box (doto (THREE/Box3.) (.setFromObject scene-obj))
-                         size (.getSize temp-box (THREE/Vector3.))
-                         max-dim (js/Math.max (.-x size) (.-y size) (.-z size))
-                         scale-factor (if (> max-dim 0) (/ 5 max-dim) 1)
-                         _ (-> scene-obj .-scale
-                               (.set scale-factor scale-factor scale-factor))
-                         box (doto (THREE/Box3.) (.setFromObject scene-obj))
-                         center (.getCenter box (THREE/Vector3.))]
-                     ; Position model to be centered and sit on the ground plane
-                     (let [base-y (- (- (.-y (.-min box))
-                                        (if static 0 1)))]
-                       (-> scene-obj .-position (.set (- (.-x center))
-                                                      base-y
-                                                      (- (.-z center))))
-                       (.add scene scene-obj)
-                       (swap! state assoc
-                              :model scene-obj
-                              :static static
-                              :model-base-y base-y))
-
-                     (dotimes [_ 15]
-                       (load-and-place-scenery scene loader
-                                               (rand-nth scenery-models)))
-
-                     ; Update controls to look at the model's new center
-                     (.updateWorldMatrix scene-obj true)
-                     (let [new-box (doto (THREE/Box3.) (.setFromObject scene-obj))
-                           new-center (.getCenter new-box (THREE/Vector3.))]
-                       (-> controls .-target (.copy new-center))
-                       (.update controls))))))]
+        loader (GLTFLoader.)]
 
     (reset! state {:scene scene
                    :camera camera
                    :renderer renderer
-                   :controls controls})
+                   :controls controls
+                   :loader loader
+                   :models []
+                   :current-model-index -1})
 
-    (print "state:" @state)
+    (set-loading true)
+    (p/let [response (js/fetch "models/directory.json")
+            dir-data (when (.-ok response) (.json response))]
+      (if dir-data
+        (let [model-files (->> (aget (first dir-data) "contents")
+                               (filter #(and (= (aget % "type") "file")
+                                             (.endsWith (aget % "name") ".glb")))
+                               (map #(aget % "name"))
+                               (map #(.substring % 2))
+                               (sort)
+                               (vec))]
+          (if (pos? (count model-files))
+            (do
+              (swap! state assoc
+                     :models model-files
+                     :current-model-index 0)
+              (load-model (first model-files)))
+            (set-loading false)))
+        (do
+          (js/console.error "Failed to load models/directory.json")
+          (set-loading false))))
 
     (.addEventListener js/window "resize" on-window-resize false)
     (on-window-resize)
+    (.addEventListener js/window "keydown" handle-key-down false)
+    (.addEventListener js/window "click" handle-tap false)
 
     (animate)))
 
